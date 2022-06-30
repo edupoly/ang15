e1214817ee5bf2d58edd9fee799a9c1b7d68671c
edupoly <info.edupoly@gmail.com>
2022-06-30 08:27:32 +0530

    todobasic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl:"./app.component.html",
 })
 export class AppComponent {
-
+  todos:any=[];
+  newtodo='';
+  addTodo(){
+    this.todos.push(this.newtodo);
+    console.log(this.todos)
+  }
 }
