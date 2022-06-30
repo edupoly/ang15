750ec987f9b1a5b455af8af49f5dc83c58cad422
edupoly <info.edupoly@gmail.com>
2022-06-30 07:45:49 +0530

    basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'myroot',
+  templateUrl:"./app.component.html",
 })
 export class AppComponent {
+  gender=null;
+  selectedTech='';
   title = 'ang15';
+  students = ['rahul','divya','priya','kumar','himaja','gauri','thirumala'];
+  mydetails = {
+    firstname:'praveen',
+    lastname:'gubbala',
+    age:39
+  }
+  employees = [
+    {
+      ename:'rohit',
+      salary:34000
+    },
+    {
+      ename:'tamoli',
+      salary:74000
+    },
+    {
+      ename:'ravali',
+      salary:72200
+    }
+  ]
 }
