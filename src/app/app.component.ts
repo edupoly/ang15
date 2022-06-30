c2e1b9931e3f9f391d88b0bb4355293d41142e3c
edupoly <info.edupoly@gmail.com>
2022-06-30 08:16:27 +0530

    basic eventhandling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl:"./app.component.html",
 })
 export class AppComponent {
-
+  abc(){
+    alert("HI")
+  }
+  ar = [12,28,22,19,25];
+  xyz(v:any){
+    alert(v)
+  }
 }
