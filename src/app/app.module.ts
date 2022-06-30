128276e6f36d444504d66859c971be38ac96ed91
edupoly <info.edupoly@gmail.com>
2022-06-30 07:47:31 +0530

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +9,6 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
