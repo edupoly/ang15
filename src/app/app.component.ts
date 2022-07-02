5950b5159bd5602528a6234ea05c2e593682a725
edupoly <info.edupoly@gmail.com>
2022-07-02 08:31:07 +0530

    baseshell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,27 +5,5 @@ import { Component } from '@angular/core';
   templateUrl:"./app.component.html",
 })
 export class AppComponent {
-  gender=null;
-  selectedTech='';
-  title = 'ang15';
-  students = ['rahul','divya','priya','kumar','himaja','gauri','thirumala'];
-  mydetails = {
-    firstname:'praveen',
-    lastname:'gubbala',
-    age:39
-  }
-  employees = [
-    {
-      ename:'rohit',
-      salary:34000
-    },
-    {
-      ename:'tamoli',
-      salary:74000
-    },
-    {
-      ename:'ravali',
-      salary:72200
-    }
-  ]
+  
 }
