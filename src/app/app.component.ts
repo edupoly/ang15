6693671efb79a2960ade8afc5ebed4a48158f3d7
edupoly <info.edupoly@gmail.com>
2022-07-05 08:04:08 +0530

    propertybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'myroot',
   templateUrl:"./app.component.html",
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  todos:any=[];
+  todos:any=[
+    {
+      title:'gym',
+      status:false
+    },
+    {
+      title:'coffee',
+      status:true
+    },
+    {
+      title:'itr',
+      status:false
+    },
+    {
+      title:'schoolfee',
+      status:true
+    },
+    {
+      title:'rent',
+      status:true
+    },
+    {
+      title:'loan',
+      status:false
+    }
+  ];
   newtodo='';
   addTodo(){
     this.todos.push(this.newtodo);
