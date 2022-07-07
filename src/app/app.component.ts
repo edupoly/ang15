5232bc34ca5b73735f6fa94e291bc9014657aa26
edupoly <info.edupoly@gmail.com>
2022-07-07 07:48:53 +0530

    ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  
+  n=123;
+  ar = [12,22,54,75,98];
+  p=346034;
+  myname='praveen'
 }
