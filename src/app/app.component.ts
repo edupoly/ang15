05f61c60d1815cf3a4ceb11d3a851a7e85c7b64c
edupoly <info.edupoly@gmail.com>
2022-07-14 08:36:22 +0530

    httpservices app countries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  
+  constructor(public http:HttpClient){}
+  countries:any=[];
+  searchKey:string='';
+  filteredCountries:any=[];
+  getCountries(){
+    this.http.get("https://restcountries.com/v2/all").subscribe((res)=>{
+      this.countries=res;
+      this.filteredCountries=res;
+    })
+  }
+  ngOnInit(){
+    this.getCountries()
+  }
+  filterBySearch(){
+    var searchResults = this.countries.filter((c:any)=>{
+      var str = JSON.stringify(c)
+      if(str.includes(this.searchKey)){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+    this.filteredCountries=searchResults;
+  }
+  sortByPopulation(){
+    this.countries.sort((a:any,b:any)=>{
+      if(a.population>b.population){
+        return -1
+      }
+      else{
+        return 1
+      }
+    })
+  }
+  sortByArea(){
+    this.countries.sort((a:any,b:any)=>{
+      if(a.area>b.area){
+        return -1
+      }
+      else{
+        return 1
+      }
+    })
+  }
 }
