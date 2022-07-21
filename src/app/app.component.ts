455fadfa843fa146056daa23ec211d7e07fee4d3
edupoly <info.edupoly@gmail.com>
2022-07-21 07:34:09 +0530

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,5 @@ import { Component } from '@angular/core';
   styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  mysal=1234.4;
-  today='3/15/2033'
-  students=[
-    {
-      firstname:'rahul',
-      age:34
-    },
-    {
-      firstname:'abhilash',
-      age:44
-    },
-    {
-      firstname:'kira',
-      age:77
-    },
-    {
-      firstname:'praveen',
-      age:39
-    },
-  ]
-  sortbyname(){
-    this.students.sort((a,b)=>{
-      return a.firstname>b.firstname?1:-1
-    })
-  }
-  sortbyage(){
-    this.students.sort((a,b)=>{
-      return a.age>b.age?1:-1
-    })
-  }
+  
 }
