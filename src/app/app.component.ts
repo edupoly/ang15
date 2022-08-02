ffafe93f708782ee7ac65cea99fc84aafee42577
edupoly <info.edupoly@gmail.com>
2022-08-02 07:15:43 +0530

    REACTIVEFORMS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'myroot',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  
+  studentForm = new FormGroup(
+    {
+      firstname:new FormControl('',[Validators.required,Validators.minLength(3)]),
+      lastname:new FormControl()
+    }
+  )
+  get firstname(){
+    return this.studentForm.controls['firstname']
+  }
+  ngOnInit(){
+    console.log(this.studentForm)
+  }
+  save(){
+    console.log(this.studentForm)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
   ],
